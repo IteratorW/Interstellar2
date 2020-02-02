dedd0d8e1877bba83f425510813a7f1eb4473c48
Rob <rob.bog@list.ru>
2020-02-02 03:12:56 +0300

    Проёб

--- is2.lua
+++ is2.lua
@@ -3,6 +3,8 @@ local GUI = require("GUI")
 local comp = require("component")
 local advancedLua = require("advancedLua")
 local fs = require("filesystem")
+local wrapper = require("is2wrapper")
+
 local gpu = comp.gpu
 
 local app = GUI.application()
@@ -10,10 +12,27 @@ local config = {
 	["firstLaunchWindow"] = true,
 	["enableAntifreeze"] = true
 }
-local colors = {}
+local colors = { 
+	mainColor = 0x0000AA,
+	desktopBackground = 0xC3C7CB,
+
+    textColor = 0x000000,
+
+    button = 0xC0C0C0,
+    buttonText = 0x000000,
+    buttonPressed = 0x8E8E8E,
+    buttonTextPressed = 0x000000,
+
+    inputBackground = 0xC0C0C0,
+    inputText = 0x383838,
+    inputPlaceholderText = 0xFFFFFF,
+    inputBackgroundFocused = 0xd1d1d1,
+    inputTextFocused = 0x000000
+}
 local windows = {}
 windows.minimized = {}
 windows.minimizedButtons = {}
+windows.apps = {}
 
 local function saveParams()
 	table.toFile("/Interstellar/params.txt", config)
@@ -31,11 +50,49 @@ local function loadParams()
 	config = table.fromFile("/Interstellar/params.txt")
 end
 
+local function iInput(x, y, width, height, backgroundColor, textColor, placeholderTextColor, backgroundFocusedColor, textFocusedColor, text, placeholderText, eraseTextOnFocus, textMask)
+	local input = GUI.input(x, y, width, height, backgroundColor, textColor, placeholderTextColor, backgroundFocusedColor, textFocusedColor, text, placeholderText, eraseTextOnFocus, textMask)
+
+	input.colors.cursor = colors.mainColor
+
+	return input
+end
+
+local function iIntInput(x, y, width, height, backgroundColor, textColor, placeholderTextColor, backgroundFocusedColor, textFocusedColor, text, placeholderText, eraseTextOnFocus, textMask)
+	local input = iInput(x, y, width, height, backgroundColor, textColor, placeholderTextColor, backgroundFocusedColor, textFocusedColor, text, placeholderText, eraseTextOnFocus, textMask)
+
+	input.validator = function(inputText)
+		return tonumber(inputText)
+	end
+
+	return input
+end
+
+local function iRangedIntInput(x, y, width, height, backgroundColor, textColor, placeholderTextColor, backgroundFocusedColor, textFocusedColor, text, placeholderText, min, max)
+	local input = iInput(x, y, width, height, backgroundColor, textColor, placeholderTextColor, backgroundFocusedColor, textFocusedColor, text, placeholderText, false, nil)
+
+	input.onValidInputFinished = function(num)
+
+	end
+
+	input.onInputFinished = function(_, input)
+		num = tonumber(input.text)
+
+		if num < min then num = min elseif num > max then num = max end
+
+		input.text = tostring(num)
+
+		input.onValidInputFinished(num)
+	end
+
+	return input
+end
+
 local function iActionButtons(x, y, width)
 	local container = GUI.container(x, y, width, 1)
 
-	container.close = container:addChild(GUI.button(1, 1, 1, 1, 0xC0C0C0, 0x000000, 0x8E8E8E, 0x000000, "―"))
-	container.minimize = container:addChild(GUI.button(container.width, 1, 1, 1, 0xC0C0C0, 0x000000, 0x8E8E8E, 0x000000, "▼"))
+	container.close = container:addChild(GUI.button(1, 1, 1, 1, colors.button, colors.buttonText, colors.buttonPressed, colors.buttonTextPressed, "―"))
+	container.minimize = container:addChild(GUI.button(container.width, 1, 1, 1, colors.button, colors.buttonText, colors.buttonPressed, colors.buttonTextPressed, "▼"))
 
 	return container
 end
@@ -43,8 +100,10 @@ end
 local function iTitledWindow(x, y, width, height, title) 
 	local window = GUI.window(x, y, width, height)
 
+	window.name = title
+
 	window.backgroundPanel = window:addChild(GUI.panel(1, 1, width, height, 0xFFFFFF))
-	window.titlePanel = window:addChild(GUI.panel(1, 1, width, 1, 0x0000AA))
+	window.titlePanel = window:addChild(GUI.panel(1, 1, width, 1, colors.mainColor))
 	window.titleLabel = window:addChild(GUI.label(1, 1, width, height, 0xFFFFFF, title)):setAlignment(GUI.ALIGNMENT_HORIZONTAL_CENTER, GUI.ALIGNMENT_VERTICAL_TOP)
 
 	window.actionButtons = window:addChild(iActionButtons(2, 2, width))
@@ -104,15 +163,14 @@ windows.removeMinimizedWindow = function(window)
 end
 
 windows.debug = function(x, y)
-	local window = iTitledWindow(x, y, 30, 10, "Debug")
-	window.name = "Debug"
+	local window = iTitledWindow(x, y, 30, 11, "Debug")
 
 	window.localY = 2
 	text = window:addChild(GUI.text(2, 2, 0x000000, "Minimized Windows: "..tostring(#windows.minimized)))
-	window:addChild(GUI.adaptiveButton(2, 4, 3, 1, 0xC0C0C0, 0x000000, 0x8E8E8E, 0x000000, "Reload")).onTouch = function()
+	window:addChild(GUI.adaptiveButton(2, 4, 3, 1, colors.button, colors.buttonText, colors.buttonPressed, colors.buttonTextPressed, "Reload")).onTouch = function()
 		text.text = "Minimized Windows: "..tostring(#windows.minimized)
 	end
-    window:addChild(GUI.adaptiveButton(17, 4, 3, 1, 0xC0C0C0, 0x000000, 0x8E8E8E, 0x000000, "Windows")).onTouch = function()
+    window:addChild(GUI.adaptiveButton(17, 4, 3, 1, colors.button, colors.buttonText, colors.buttonPressed, colors.buttonTextPressed, "Windows")).onTouch = function()
         if #windows.minimized < 1 then
             GUI.alert("No minimized windows!")
             return
@@ -121,10 +179,13 @@ windows.debug = function(x, y)
 		local data = windows.minimized[1].name or "none"
 		GUI.alert(#windows.minimized.."\n"..data)
     end
-    window:addChild(GUI.adaptiveButton(2, 8, 2, 1, 0xC0C0C0, 0x000000, 0x8E8E8E, 0x000000, "Show all")).onTouch = function()
+    window:addChild(GUI.adaptiveButton(2, 8, 2, 1, colors.button, colors.buttonText, colors.buttonPressed, colors.buttonTextPressed, "ShowAll")).onTouch = function()
 		for _, window in pairs(windows.minimized) do
             windows.removeMinimizedWindow(window)
         end
+	end
+	window:addChild(GUI.adaptiveButton(16, 8, 1, 1, colors.button, colors.buttonText, colors.buttonPressed, colors.buttonTextPressed, "IWrapperDemo")).onTouch = function()
+		wrapper.toggleDemoMode()
 	end
 
 	return window
@@ -138,7 +199,7 @@ windows.firstLaunchWindow = function(x, y)
 	window:addChild(GUI.text(2, 3, 0x000000, "Be sure to check out settings to configure IS2 or your ship."))
 	window:addChild(GUI.text(2, 4, 0x000000, "Good luck, have fun!"))
 
-	window:addChild(GUI.button(window.width-6, window.height-1, 6, 1, 0xC0C0C0, 0x000000, 0x8E8E8E, 0x000000, "OK")).onTouch = function()
+	window:addChild(GUI.button(window.width-6, window.height-1, 6, 1, colors.button, colors.buttonText, colors.buttonPressed, colors.buttonTextPressed, "OK")).onTouch = function()
 		window:remove()
 		app:addChild(windows.mainWindow(5, 5))
 
@@ -147,7 +208,7 @@ windows.firstLaunchWindow = function(x, y)
 		saveParams()
 	end
 
-	window:addChild(GUI.button(window.width-13, window.height-1, 6, 1, 0xC0C0C0, 0x000000, 0x8E8E8E, 0x000000, "Exit")).onTouch = function()
+	window:addChild(GUI.button(window.width-13, window.height-1, 6, 1, colors.button, colors.buttonText, colors.buttonPressed, colors.buttonTextPressed, "Exit")).onTouch = function()
 		app:stop()
 	end
 
@@ -158,6 +219,17 @@ windows.mainWindow = function(x, y)
 	local window = iTitledWindow(x, y, 80, 25, "Interstellar2 Main Window")
 	window.name = "Main"
 
+	count = 1
+    for _, windowApp in pairs(windows.apps) do
+		window:addChild(GUI.framedButton(2 + 12 * count - 12, 3, 12, 6, 0x000000, 0x000000, 0xFAFAFA, 0xFAFAFA, windowApp.name)).onTouch = function()
+	    	if windowApp.check() then
+				app:addChild(windowApp.getWindow(10, 10))
+			end
+		end
+
+		count = count + 1
+    end
+
 	window.actionButtons.close.onTouch = function()
 		app:stop()
 	end
@@ -165,47 +237,65 @@ windows.mainWindow = function(x, y)
 	return window
 end
 
-windows.jumpWindow = function(x, y)
-	local window = iTitledWindow(x, y, 80, 25, "Jump Window")
+windows.apps.jumpWindow = {
+	name = "Jump Menu",
 
-    local max = 20
-    local x,y,z = 1, 1, 1
-    local x2,y2,z2 = 1, 1, 1
-    local mindx = x + x2
-    local mindy = z + z2
-    local mindz = y + y2
-    local rotmax = 270
-    local jumpX, jumpY, jumpZ = 1, 1, 1
-    local rot = 0
+	check = function()
+		if not wrapper.shipApiAvailable() then
+			GUI.alert("Ship is not available.")
+			return false
+		end
 
-	window:addChild(GUI.panel(1, 1, window.width, window.height, colors.window))
-    window:addChild(GUI.label(1, 1, 61, 1, colors.button, "Jump")):setAlignment(GUI.ALIGNMENT_HORIZONTAL_CENTER, GUI.ALIGNMENT_VERTICAL_CENTER)
-    window:addChild(GUI.label(2, 4, 8, 1, 0x555555, "Entered values will be clamped automatically."))
-    window:addChild(GUI.label(2, 6, 16, 1, colors.textColor, "X ("..mindx.." - "..tostring(max + mindx)..")"))
-    window:addChild(GUI.input(2, 7, 30, 1, 0xEEEEEE, 0x555555, 0x999999, 0xFFFFFF, 0x2D2D2D, jumpX, "X")).onInputFinished = function(window, input, eventData, text)
+		return true
+	end,
 
-    end    
-    window:addChild(GUI.label(2, 9, 12, 1, colors.textColor, "Y ("..mindy.." - "..tostring(max + mindy)..")"))
-    window:addChild(GUI.input(2, 10, 30, 1, 0xEEEEEE, 0x555555, 0x999999, 0xFFFFFF, 0x2D2D2D, jumpY, "Y")).onInputFinished = function(window, input, eventData, text)
+	getWindow = function(x, y)
+		local window = iTitledWindow(x, y, 62, 20, "Jump Window")
 
-    end    
-    window:addChild(GUI.label(2, 12, 14, 1, colors.textColor, "Z ("..mindz.." - "..tostring(max + mindz)..")"))
-    window:addChild(GUI.input(2, 13, 30, 1, 0xEEEEEE, 0x555555, 0x999999, 0xFFFFFF, 0x2D2D2D, jumpZ, "Z")).onInputFinished = function(window, input, eventData, text)
+		local max = wrapper.ship.getMaxJumpDistance()
+		local pX, pY, pZ = wrapper.ship.getDimPositive()
+	    local nX, nY, nZ = wrapper.ship.getDimNegative()
 
-    end    
-    window:addChild(GUI.label(2, 15, 14, 1, colors.textColor, 'CCW rotation (90 deg step)'))
-    window:addChild(GUI.input(2, 16, 30, 1, 0xEEEEEE, 0x555555, 0x999999, 0xFFFFFF, 0x2D2D2D, "0", "R")).onInputFinished = function(window, input, eventData, text)
+	    local maxX = max + pX + nX
+	    local maxY = max + pY + nY
+	    local maxZ = max + pZ + nZ
 
-    end
-    window:addChild(GUI.button(2, 18, 29, 3, colors.button, colors.textColor2, colors.buttonPressed, colors.textColor2, "Jump")).onTouch = function()
+	    local rotmax = 270
+	    local jumpX, jumpY, jumpZ = wrapper.ship.getMovement()
+	    local rot = 0
 
-    end
-    window:addChild(GUI.button(33, 18, 29, 3, colors.button, colors.textColor2, colors.buttonPressed, colors.textColor2, "Hyper")).onTouch = function()
+	    window:addChild(GUI.label(2, 3, 8, 1, 0x555555, "Entered values will be automatically limited."))
+	    window:addChild(GUI.label(2, 5, 16, 1, colors.textColor, string.format("X (%s - %s)", pX + nX, maxX)))
+	    window:addChild(iRangedIntInput(2, 6, 30, 1, colors.inputBackground, colors.inputText, colors.inputPlaceholderText, colors.inputBackgroundFocused, colors.inputTextFocused, jumpX, "X", -maxX, maxX)).onValidInputFinished = function(num)
+	    	junpX = num
+	    end    
 
-    end
+	    window:addChild(GUI.label(2, 8, 12, 1, colors.textColor, string.format("Y (%s - %s)", pY + nY, maxY)))
+	    window:addChild(iRangedIntInput(2, 9, 30, 1, colors.inputBackground, colors.inputText, colors.inputPlaceholderText, colors.inputBackgroundFocused, colors.inputTextFocused, jumpY, "Y", -maxY, maxY)).onValidInputFinished = function(num)
+	    	jumpY = num
+	    end    
 
-    return window
-end
+	    window:addChild(GUI.label(2, 11, 14, 1, colors.textColor, string.format("Z (%s - %s)", pZ + nZ, maxZ)))
+	    window:addChild(iRangedIntInput(2, 12, 30, 1, colors.inputBackground, colors.inputText, colors.inputPlaceholderText, colors.inputBackgroundFocused, colors.inputTextFocused, jumpZ, "Z", -maxZ, maxZ)).onValidInputFinished = function(num)
+	    	jumpZ = num
+	    end    
+
+	    window:addChild(GUI.label(2, 14, 14, 1, colors.textColor, 'Clockwise rotation (90° step)'))
+	    window:addChild(iIntInput(2, 15, 30, 1, colors.inputBackground, colors.inputText, colors.inputPlaceholderText, colors.inputBackgroundFocused, colors.inputTextFocused, "0", "R", -270, 270)).onValidInputFinished = function(num)
+	    	rot = num
+	    end
+
+	    window:addChild(GUI.button(2, 17, 29, 3, colors.button, colors.buttonText, colors.buttonPressed, colors.buttonTextPressed, "Jump")).onTouch = function()
+	    	wrapper.ship.jump(rot, jumpX, jumpY, jumpZ, false)
+	    end
+
+	    window:addChild(GUI.button(33, 17, 29, 3, colors.button, colors.buttonText, colors.buttonPressed, colors.buttonTextPressed, "Hyperspace jump")).onTouch = function()
+	    	wrapper.ship.jump(nil, nil, nil, nil, true)
+	    end
+
+	    return window
+	end
+}
 
 ------------------------------------------------------------------------
 local maxRes = {}
@@ -225,8 +315,9 @@ if (res[1] < 100) and (res[2] < 40) then
 end
 buffer.setResolution(res[1], res[2])
 
-app:addChild(GUI.panel(1, 1, app.width, app.height, 0xC3C7CB))
-windows.iDebug = app:addChild(GUI.textBox(app.width-33, 2, 32, 16, 0xC0C0C0, 0xFFFFFF, {"Debug info:"}, 1, 1, 0))
+app:addChild(GUI.panel(1, 1, app.width, app.height, colors.desktopBackground))
+
+windows.iDebug = app:addChild(GUI.textBox(app.width-33, 2, 32, 16, colors.button, 0xFFFFFF, {"Debug info:"}, 1, 1, 0))
 
 if config.firstLaunchWindow then
 	app:addChild(windows.firstLaunchWindow(5, 5))
